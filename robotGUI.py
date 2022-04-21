470965e659ea7dc7100e04b2e02c14967d24017e
Braeden Hunt <bjhunt918@gmail.com>
2022-04-21 17:03:46 -0600

    Fixed Sizing Issue

--- robotGUI.py
+++ robotGUI.py
@@ -197,8 +197,6 @@ class RobotGUI(tk.Tk):
         self.anim = None
         self.gif_label = tk.Label(self.animation_window, image="")
         self.gif_label.pack()
-        self.animation_window.geometry("800x480")
-
 
         self.animation_window.grab_set()
         self.animation()
