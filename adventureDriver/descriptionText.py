91914a2a6a3c9620cd2d9aa414db4ba58bab88d9
Autoson1009 <autoson@comcast.net>
2022-04-29 12:04:02 -0600

    Added adventure driver code

---
+++ adventureDriver/descriptionText.py
@@ -0,0 +1,60 @@
+startDesc = "You awake in an empty dungeon cell... how will you escape?\nBeware, as you only have limited time to make it out!"
+
+startRoomDesc = "An empty cell, you started here."
+
+endRoomDesc = "There is a massive locked door that blocks the way out."
+
+timeLoseText = "Succumbing to fatigue, you collapse. OUT OF TIME: YOU HAVE FAILED"
+
+monsterLoseText = "The monster strikes a fatal blow! YOU HAVE FAILED"
+
+trapLoseText = "The deadly trap has been your doom! YOU HAVE FAILED"
+
+winText = "The key turns in the door, opening the way to freedom. YOU WIN"
+
+roomIntros = [
+    "You see ",
+    "Here, there is ",
+    "Ahead you can see ",
+    "Looming up ahead, ",
+    "You find ",
+    "The next room is ",
+    "There is ",
+    "You find yourself at "
+]
+
+roomDescs = [
+    "a dusty, torchlit hallway.",
+    "a large, pillared throne room.",
+    "an echoing, dark cavern.",
+    "a grimey, torchlit cell.",
+    "a dank and moldy vault.",
+    "some slippery steps leading down.",
+    "a chilled, swampy chasm.",
+    "a high-ceilinged, echoing chapel.",
+    "a grim, candle-lit ossuary.",
+    "a reeking, refuse-strewn cesspit.",
+    "an imposing bastion of blackened iron.",
+    "a ruined heap of crumbled marble.",
+    "a long-abandoned workshop of tarnished brass.",
+    "a papery-walled, buzzing insect hive.",
+    "a cramped, malodorous burrow.",
+    "a lonely, lofted bridge of narrow brick.",
+    "a great, yawning pit.",
+    "a room filled with rugs of every color.",
+    "a courtyard of flickering candles.",
+    "a twisting hall of mirrors.",
+    "an abandoned bakery that still smells of bread.",
+    "a domed chamber painted with stars.",
+    "a frightening portrait gallery.",
+    "a decrepit catacomb, filled with bones",
+    "an old dungeon, skeletons hanging from shackles",
+    "a set of uneven steps, leading upwards.",
+    "a grand cathedral with ornate pillars.",
+    "a dimly lit circular room with many doors",
+    "a well lit entryway with colorful tiled floors",
+    "a neverending tunnel with red lanterns",
+    "a dazzling open room filled with exquisite pews",
+    "a hazy crawlspace with slimy floors",
+    "a musty archive filled with old papers."
+]
