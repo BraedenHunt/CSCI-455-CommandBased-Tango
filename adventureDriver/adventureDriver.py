c90eb640e6fffd40e18c19a51a24130790e61f2a
Braeden Hunt <bjhunt918@gmail.com>
2022-04-29 12:30:39 -0600

    Fixed Import Issue

--- adventureDriver/adventureDriver.py
+++ adventureDriver/adventureDriver.py
@@ -1,10 +1,170 @@
-from mapVariable import *
-from descriptionText import *
 import random
 import os
 import sys
 
+startDesc = "You awake in an empty dungeon cell... how will you escape?\nBeware, as you only have limited time to make it out!"
 
+startRoomDesc = "An empty cell, you started here."
+
+endRoomDesc = "There is a massive locked door that blocks the way out."
+
+timeLoseText = "Succumbing to fatigue, you collapse. OUT OF TIME: YOU HAVE FAILED"
+
+monsterLoseText = "The monster strikes a fatal blow! YOU HAVE FAILED"
+
+trapLoseText = "The deadly trap has been your doom! YOU HAVE FAILED"
+
+winText = "The key turns in the door, opening the way to freedom. YOU WIN"
+
+roomIntros = [
+    "You see ",
+    "Here, there is ",
+    "Ahead you can see ",
+    "Looming up ahead, ",
+    "You find ",
+    "The next room is ",
+    "There is ",
+    "You find yourself at "
+]
+
+roomDescs = [
+    "a dusty, torchlit hallway.",
+    "a large, pillared throne room.",
+    "an echoing, dark cavern.",
+    "a grimey, torchlit cell.",
+    "a dank and moldy vault.",
+    "some slippery steps leading down.",
+    "a chilled, swampy chasm.",
+    "a high-ceilinged, echoing chapel.",
+    "a grim, candle-lit ossuary.",
+    "a reeking, refuse-strewn cesspit.",
+    "an imposing bastion of blackened iron.",
+    "a ruined heap of crumbled marble.",
+    "a long-abandoned workshop of tarnished brass.",
+    "a papery-walled, buzzing insect hive.",
+    "a cramped, malodorous burrow.",
+    "a lonely, lofted bridge of narrow brick.",
+    "a great, yawning pit.",
+    "a room filled with rugs of every color.",
+    "a courtyard of flickering candles.",
+    "a twisting hall of mirrors.",
+    "an abandoned bakery that still smells of bread.",
+    "a domed chamber painted with stars.",
+    "a frightening portrait gallery.",
+    "a decrepit catacomb, filled with bones",
+    "an old dungeon, skeletons hanging from shackles",
+    "a set of uneven steps, leading upwards.",
+    "a grand cathedral with ornate pillars.",
+    "a dimly lit circular room with many doors",
+    "a well lit entryway with colorful tiled floors",
+    "a neverending tunnel with red lanterns",
+    "a dazzling open room filled with exquisite pews",
+    "a hazy crawlspace with slimy floors",
+    "a musty archive filled with old papers."
+]
+
+adventureMap = [
+    {   #room 1
+        'east':2
+    },
+    {   #room 2
+        'south':7,
+        'east':3,
+        'west':1
+    },
+    {   #room 3
+        'south':8,
+        'west':2
+    },
+    {   #room 4
+        'east':5
+    },
+    {   #room 5
+        'west':4,
+        'south':10
+    },
+    {   #room 6
+        'east':7,
+        'south':11
+    },
+    {   #room 7
+        'north':2,
+        'west':6,
+        'south':12
+    },
+    {   #room 8
+        'north':3
+    },
+    {   #room 9
+        'east':10,
+        'south':14
+    },
+    {   #room 10
+        'north':5,
+        'west':9,
+        'south':15
+    },
+    {   #room 11
+        'north':6,
+        'south':16
+    },
+    {   #room 12
+        'north':7,
+        'east':13
+    },
+    {   #room 13
+        'east':14,
+        'west':12
+    },
+    {   #room 14
+        'west':13,
+        'north':9,
+        'south':19
+    },
+    {   #room 15
+        'north':10,
+        'south':20
+    },
+    {   #room 16
+        'north':11,
+        'east':17
+    },
+    {   #room 17
+        'west':16,
+        'south':22
+    },
+    {   #room 18
+        'south':23
+    },
+    {   #room 19
+        'north':14,
+        'south':24,
+        'east':20
+    },
+    {   #room 20
+        'west':19,
+        'north':15
+    },
+    {   #room 21
+        'east':22
+    },
+    {   #room 22
+        'west':21,
+        'north':17,
+        'east':23
+    },
+    {   #room 23
+        'west':22,
+        'north':18
+    },
+    {   #room 24
+        'east':25,
+        'north':19
+    },
+    {   #room 25
+        'west':24
+    }
+]
 
 ###############################################################################################
 # Classes
